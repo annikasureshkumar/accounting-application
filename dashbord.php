d1c656698b7bbdbb4caa8657e285537d47ed1f55
ankamswathi <47879114+ankamswathi@users.noreply.github.com>
2019-04-15 15:03:25 +0530

    Add files via upload

---
+++ dashbord.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Account Category</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="dashboardcss.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <div class="container">
+  <form action="">
+          <div class="alert alert-primary text-center bg-danger">
+            <h1>DASHBOARD</h1>
+          </div>
+          <div class=" text-center text-danger">
+            <h2>TRANSACTIONS</h2>
+          </div>
+          <!--end of header-->
+         
+       
+          <div class="row">
+            <div class="col-2">
+              <nav class="nav flex-column ">
+                <h5><a class="nav-link" href="#">Users</a></h5>
+                <h5><a class="nav-link" href="#">Attachments</a></h5>
+                <h5><a class="nav-link" href="#">Categories</a></h5>
+                <h5><a class="nav-link" href="#">Transactions</a></h5>
+                <h5><a class="nav-link" href="#">List of Account</a></h5>
+              </nav>
+            </div>
+            
+            <div class="col-8 text-center text-success">
+              <div class="flex-column">
+             <h4>Accounts</h4>
+             <h4>Reports</h4>
+             </div>
+            </div>
+            <div class="col">
+            <div class="row">
+            <div class="col">
+              <nav class="nav flex-column">
+                <h5><a class="nav-link text-right" href="#">Income</a></h5>
+                <h5><a class="nav-link text-right" href="#">Expense</a></h5>
+                <h5><a class="nav-link text-right" href="#">Total</a></h5>
+                <h5><a class="nav-link text-right" href="#">Daily</a></h5>
+                <h5><a class="nav-link text-right" href="#">Monthly</a></h5>
+                <h5><a class="nav-link text-right" href="#">Chart</a></h5>
+              </nav>
+            </div>
+            </div>
+          </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</form> 
+</div>
+</body>
+</html>
